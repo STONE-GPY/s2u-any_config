75c1d791163095f87e535164a38550b496ec0df4
Wend4r <47463683+Wend4r@users.noreply.github.com>
2024-07-01 08:42:09 +0300

    Push JSON reader/writer

---
+++ include/any_config/json/writer.hpp
@@ -0,0 +1,44 @@
+/**
+ * vim: set ts=4 sw=4 tw=99 noet :
+ * ======================================================
+ * Source 2 util to read a config of any format.
+ * Written by Wend4r (2024).
+ * ======================================================
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef _INCLUDE_ANY_CONFIG_JSON_WRITER_HPP_
+#define _INCLUDE_ANY_CONFIG_JSON_WRITER_HPP_
+
+#pragma once
+
+#include "types.hpp"
+
+#include <any_config/base/base.hpp>
+
+namespace AnyConfig
+{
+	class CJSONWriter : public CBase
+	{
+	public:
+		//
+		// Save ones.
+		//
+		bool SaveJSON(const SaveJSON_t &aParams);
+		bool SaveJSON(const SaveJSON2_t &aParams);
+	}; // CJSONWriter
+}; // AnyConfig
+
+#endif // _INCLUDE_ANY_CONFIG_JSON_WRITER_HPP_
---
+++ src/any_config/json/writer.cpp
@@ -0,0 +1,38 @@
+/**
+ * vim: set ts=4 sw=4 tw=99 noet :
+ * ======================================================
+ * Source 2 util to read a config of any format.
+ * Written by Wend4r (2024).
+ * ======================================================
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <any_config/json/writer.hpp>
+
+#include <tier0/keyvalues3.h>
+
+bool AnyConfig::CJSONWriter::SaveJSON(const SaveJSON_t &aParams)
+{
+	return SaveKV3AsJSON(aParams.CSaveFrom_t<KeyValues3 *>::aData, 
+	                     aParams.psMessage, 
+	                     aParams.COutput_t<CUtlBuffer *>::aData);
+}
+
+bool AnyConfig::CJSONWriter::SaveJSON(const SaveJSON2_t &aParams)
+{
+	return SaveKV3AsJSON(aParams.CSaveFrom_t<KeyValues3 *>::aData, 
+	                     aParams.psMessage, 
+	                     aParams.COutput_t<CUtlString *>::aData);
+}
