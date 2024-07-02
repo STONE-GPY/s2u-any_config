29418a5e70dffa965cede5040732a9f39d45865d
Wend4r <47463683+Wend4r@users.noreply.github.com>
2024-07-02 19:36:42 +0300

    Add KeyValues include

---
+++ include/any_config/keyvalues.hpp
@@ -0,0 +1,37 @@
+/**
+ * vim: set ts=4 sw=4 tw=99 noet :
+ * ======================================================
+ * Source 2 util to read a config of any format.
+ * Written by Wend4r (2024).
+ * ======================================================
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef _INCLUDE_ANY_CONFIG_KEYVALUES_HPP_
+#define _INCLUDE_ANY_CONFIG_KEYVALUES_HPP_
+
+#pragma once
+
+#include "keyvalues/reader.hpp"
+#include "keyvalues/writer.hpp"
+
+namespace AnyConfig
+{
+	class KeyValues : virtual public CKeyValuesReader, virtual public CKeyValuesWriter
+	{
+	}; // KeyValues
+}; // AnyConfig
+
+#endif // _INCLUDE_ANY_CONFIG_KEYVALUES_HPP_
