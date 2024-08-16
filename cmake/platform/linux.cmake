30383efa4268caf0e048315da78f0749a11e7106
Wend4r <47463683+Wend4r@users.noreply.github.com>
2024-08-16 23:24:54 +0300

    Update CMake things

--- cmake/platform/linux.cmake
+++ cmake/platform/linux.cmake
@@ -2,19 +2,28 @@
 # Copyright (C) 2024 Wend4r
 # Licensed under the GPLv3 license. See LICENSE file in the project root for details.
 
-# Warnings
 set(PLATFORM_COMPILER_OPTIONS
-	-Wall -Wno-uninitialized -Wno-switch -Wno-unused
-	-Wno-conversion-null -Wno-write-strings
-	-Wno-invalid-offsetof -Wno-reorder
-	-Wno-register
+	${PLATFORM_COMPILER_OPTIONS}
 
-	# Others
-	-mfpmath=sse -msse -fno-strict-aliasing
-	-fno-threadsafe-statics -fvisibility=default
+	-Wall
+	-Wno-conversion -Wno-overloaded-virtual -Wno-invalid-offsetof
+	-Wno-non-virtual-dtor -Wno-delete-non-virtual-dtor
+	-Wno-unused -Wno-register
+	-Wno-narrowing
+	-Wno-ordered-compare-function-pointers
+
+	-mtune=generic -mmmx -msse -msse2 -msse3 -msse4 -msse4.1
+	-fvisibility=default -fPIC
+
+	-fno-strict-aliasing -fno-threadsafe-statics
 )
 
-set(PLATFORM_LINKER_OPTIONS)
+set(PLATFORM_LINKER_OPTIONS
+	${PLATFORM_LINKER_OPTIONS}
+)
+
 set(PLATFORM_COMPILE_DEFINITIONS
+	${PLATFORM_COMPILE_DEFINITIONS}
+
 	-D_GLIBCXX_USE_CXX11_ABI=0
 )
